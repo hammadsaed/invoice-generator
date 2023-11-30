2a85b0944d3deee0a0ab582c621d12bb7bb28364
Hammad Saeed <hammad.saeed776@gmail.com>
2023-11-30 07:17:34 +0500

    Add invoice components

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
     return (
       <div className="App d-flex flex-column align-items-center justify-content-center w-100">
         <Container>
+          <InvoiceForm />
         </Container>
       </div>
     );
